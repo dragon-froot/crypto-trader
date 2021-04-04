db43e2932377b79cbd000feb50fbd93f88f7b949
Mason C <dragon-froot710@protonmail.com>
2021-04-03 22:48:49 -0700

    Working on market data while I figure out the auth Issue

--- market_data.py
+++ market_data.py
@@ -10,18 +10,32 @@ class CoinData:
         response = requests.get('{0}/products/{1}/book'.format(self.url, self.symbol)).json()
         return response
 
+    # Get historical data for a given time range
+    def get_historical_data(self):
+        pass
 
 
-# Make sure to provide a ticker and a timeout time
-def monitor_pricing(symbol, timeout):
-    price = CoinData(symbol)
 
-    print(price.get_market_data())
-    time.sleep(timeout)
+
+class Monitor:
+    def __init__(self, symbol, timeout):
+        self.symbol = symbol
+        self.timeout = timeout
+    # Make sure to provide a ticker and a timeout time
+    def monitor_pricing(self):
+        price = CoinData(self.symbol)
+
+        print(price.get_market_data())
+        time.sleep(self.timeout)
+
+# This function will grab the a value every X ammount of time and store it 
+def store_info(tp, currentCoinObj, previousCoinObj):
+    timePeriod = tp
+
 
 
 while True:
-    monitor_pricing("BTC-USD", 5)
+    Monitor("BTC-USD", 5).monitor_pricing()
 
 
 
