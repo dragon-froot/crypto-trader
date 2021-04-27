a50f3b9c0b44347ff4fc75dda8b91a08c4fa98ac
Mason C <dragon-froot710@protonmail.com>
2021-04-26 23:49:52 -0700

    Removed authentication from MarketData rotues

--- MarketData.py
+++ MarketData.py
@@ -1,14 +1,9 @@
 import time, requests
 from urls import *
 from auth_client import CBAuth
-from auth_credentials import PASSPHRASE, KEY, SECRET
 
 class MarketData:
-    def __init__(self):
-        # self.currency = currency
-        self.auth = CBAuth(KEY,SECRET,PASSPHRASE)
     
-
     def currencies(self):
         response = requests.get(get_currencies()).json()
 
@@ -21,27 +16,27 @@ class MarketData:
     
 
     def product_from_id(self, product_id):
-        response = requests.get(get_product_by_id(product_id), auth=self.auth).json()
+        response = requests.get(get_product_by_id(product_id)).json()
 
         return response
     
     def product_order_book(self, product_id, level):
-        response = requests.get(get_product_order_book(product_id, level), auth=self.auth).json()
+        response = requests.get(get_product_order_book(product_id, level)).json()
 
         return response
 
     def product_trades(self, product_id):
-        response = requests.get(get_product_trades(product_id), auth=self.auth).json()
+        response = requests.get(get_product_trades(product_id)).json()
 
         return response
     
     def get_historic_rates(self, product_id, start, end, timeslice):
         url = get_historic_trades(product_id, start, end, timeslice)
-        response = requests.get(url, auth=self.auth).json()
+        response = requests.get(url).json()
 
         return response
 
     def get_24hr_stats(self, product_id):
-        response = requests.get(get_24hr_stats(product_id), auth=self.auth).json()
+        response = requests.get(get_24hr_stats(product_id)).json()
 
         return response
