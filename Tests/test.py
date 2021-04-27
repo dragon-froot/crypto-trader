a651cbc4e8482ad8e68efae8c8873bd1b74cfc7c
Mason C <dragon-froot710@protonmail.com>
2021-04-26 23:47:40 -0700

    Working on tests

--- Tests/test.py
+++ Tests/test.py
@@ -4,9 +4,19 @@ from auth_clientu import CBAuth
 from auth_credentials import PASSPHRASE, KEY, SECRET
 from urls import *
 
-# Global variables
-auth = CBAuth(KEY, SECRET, PASSPHRASE)
-accountData = Account()
-
 class Test(MarketData):
-        
+    def __init__(self):
+        self.product_id = 'BTC'
+        self.marketDataFunctionList = list(
+                self.currencies(),
+                self.products(),
+                self.product_from_id(self.product_id),
+                self.product_order_book(self.product_id, 3),
+                self.product_trades(self.product_id),
+                self.get_historic_rates(self.product_id),
+                self.get_24hr_stats(self.product_id)
+            )
+        self.auth = CBAuth(KEY, SECRET, PASSPHRASE)accountData = Account()
+    
+    def test_market_data_functions(self):
+        pass
